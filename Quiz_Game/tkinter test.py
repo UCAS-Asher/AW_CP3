bc8db8b2882209b9f8520932c46b3db6cc075b85
UCAS-Asher <asher.wangia@ucas-edu.net>
2025-08-20 11:48:51 -0600

    tkinter

--- Quiz_Game/tkinter test.py
+++ Quiz_Game/tkinter test.py
@@ -1,24 +1,6 @@
 import tkinter as tk
+import time
 
-def clear_frame(frame):
-    for widget in frame.winfo_children():
-        widget.destroy()
-
-#clear_screen = clear_frame(main_frame)
-
-def typewriter_effect(label, text, index=0, delay=50):
-    """
-    Creates a typing effect for a Tkinter Label widget.
-
-    Args:
-        label (tk.Label): The Label widget to display the text.
-        text (str): The full text to be typed.
-        index (int): The current character index being displayed.
-        delay (int): The delay in milliseconds between each character.
-    """
-    if index < len(text):
-        label.config(text=text[:index+1])  # Update text with one more character
-        label.after(delay, typewriter_effect, label, text, index + 1, delay)
 
 
 def main():
@@ -27,19 +9,49 @@ def main():
     main_frame = tk.Frame(root)
     main_frame.pack(padx=480, pady=270)
 
+    def clear_frame(frame):
+        for widget in frame.winfo_children():
+            widget.destroy()
+    
+    def typewriter_effect(label, text, index=0, delay=60):
+        """
+        Creates a typing effect for a Tkinter Label widget.
 
+        Args:
+            label (tk.Label): The Label widget to display the text.
+            text (str): The full text to be typed.
+            index (int): The current character index being displayed.
+            delay (int): The delay in milliseconds between each character.
+        """
+        if index < len(text):
+            label.config(text=text[:index+1])  # Update text with one more character
+            label.after(delay, typewriter_effect, label, text, index + 1, delay)
+
+ 
 
     
-    label = tk.Label(root, text="", font=("Arial", 20), fg="blue")
-    label_2 = tk.Label(root, text="", font=("Arial", 16), fg="blue")
+    label = tk.Label(root, text="", font=("Arial", 20), fg="blue", wraplength=420)
 
     label.place(x=10, y=10)
 
-    text_display_1 = "Hello There, This is a Quiz Game"
+    text_display_1 = "Hello There, This is a Quiz Game Do you want to Play The Game?"
     typewriter_effect(label, text_display_1)
 
-    text_display_2 = "Do you want to Play The Game?"
-    typewriter_effect(label, text_display_2)
+    button_exit = tk.Button(root, text="No")
+    button_play = tk.Button(root, text="Yes")
+
+    button_play.place(x= 20, y= 90, width=25, command=)
+    button_exit.place(x= 60, y= 90, width=25, command=exit())
+
+
+
+    
+    
+    
+    
+  
+
+    
 
 
 
