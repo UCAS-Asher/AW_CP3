98baafa1924da88b97a776b30c692e96956868e7
UCAS-Asher <asher.wangia@ucas-edu.net>
2025-08-26 21:40:51 -0600

    Working On It

--- User_Login/main.cpp
+++ User_Login/main.cpp
@@ -5,12 +5,29 @@
 using namespace std;
 
 int main(){
-    string admins[4] = {"Mango6", "Mustard7", "MangoMustard67",};
+    string admins[3] = {"Mango6", "Mustard7", "MangoMustard67"};
     string old_users[5] = {"MangoMustard", "MustardMango", "Mango", "Mustard", "Hello67"};
     int choice;
     cout << "User Login" << endl << "1. Sign Up" << endl << "2. Login To Existing" << endl << "Choose a Number: ";
     cin >> choice;
     
-    
+    if (choice == 1){
+        string new_name;
+        cout << "What Username do you want to sign up as: ";
+        cin >> new_name;
+        cout << endl << "Hello New User " << new_name;
+        main();
+    }
+    else if (choice == 2){
+        string user_name;
+        cout << "Whats your Username: ";
+        cin >> user_name;
+        int admin = admins.find(user_name);
+        int user = old_users.find(user_name);
+
+    }
+    else{
+        cout << "Program End";
+    }
     return 0;
 }
