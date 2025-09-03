38f4064655ce79f397d5956d6a9d0beb785c5da4
UCAS-Asher <asher.wangia@ucas-edu.net>
2025-09-03 11:49:15 -0600

    Working On It

--- Tic_Tac_Toe/main.cpp
+++ Tic_Tac_Toe/main.cpp
@@ -5,7 +5,7 @@
 using namespace std;
 
 
-void print_board(int board[3][3]){
+void print_board(string board[3][3]){
     for(int y = 0; y < 3; y++){
         cout << endl;
         for(int x = 0; x < 3; x++){
@@ -15,41 +15,56 @@ void print_board(int board[3][3]){
     cout << endl;
 }
 
-int user_choice(){
-    int user;
+string user_choice(){
+    string user;
     cout << "Its Your Turn. Choose a Number: ";
     cin >> user;
     cout << endl;
 
-    if(user == 1|| user == 2|| user == 3|| user == 4|| user == 5|| user == 6|| user == 7|| user == 8|| user == 9){
+    if(user == "1"|| user == "2"|| user == "3"|| user == "4"|| user == "5"|| user == "6"|| user == "7"|| user == "8"|| user == "9"){
         return user;
     }
     else{
         cout << "Not a Choice!" << endl;
-        user_choice();
+        string user = user_choice();
+        return user;
     }
 }
 
-int place_check(int place, int board[3][3]){
-    int options[9];
-    
+int check_place(string place, string board[3][3]){   
+    int place_good = 0;
+
     for(int y = 0; y < 3; y++){
         for(int x = 0; x < 3; x++){
-            if(to_string(board[y][x]) != "X" && to_string(board[y][x]) != "O"){
-                options.add()
+            if(place == board[y][x]){
+                place_good+=1;
             }
+           
         }
     }
-
+    return place_good;
 }
 
-void user_place(){}
+void get_user_place(string board[3][3]){
+    string place = user_choice();
+    int place_good = check_place(place, board);
+}
+
+void place_x(){
+    
+}
+
+void user_place(string board[3][3]){
+    get_user_place(board);
+    place_x();
+}
 
 void game(){
-    int board[3][3] = {{1,2,3}, {4,5,6}, {7,8,9}};
+    string board[3][3] = {{"1","2","3"}, {"4","5","6"}, {"7","8","9"}};
     cout << endl << "You are X" << endl;
     print_board(board);
-    user_place();
+    user_place(board);
+    print_board(board);
 }
 
 
