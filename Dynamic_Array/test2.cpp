0387e8d055c67175f0a186fe591c5628e800af10
UCAS-Asher <asher.wangia@ucas-edu.net>
2025-09-15 11:48:21 -0600

    Finished

--- Dynamic_Array/test2.cpp
+++ Dynamic_Array/test2.cpp
@@ -1,40 +1,66 @@
 
 #include <iostream>
-#include <memory>
+#include <memory> // Required for std::unique_ptr
+#include <algorithm> // Required for std::copy
 using namespace std;
 
-int capacity = 1;
-int entries = 0;
+bool check_if_digit(const string& str){
+    for (char i : str) {
+        if (isdigit(i)) {
+            return true;
+        }
+    }
+    return false;
+}
 
 
-int main(){
-
-    unique_ptr <string> colors(new string[capacity]);
-    *colors = new string[capacity];
+void program(){
+    int space = 1;
+    unique_ptr<string[]> colors = make_unique<string[]>(space);
+    int entries = 0;
 
     while(true){
         cout << "Number: ";
         cin >> colors[entries];
-        if (cin.fail()) break;
-            entries++;
-        if(entries == capacity){
-            capacity += 1;
-            int* temp = new int[capacity];
-            for(int i = 0; i < entries; i++)
-                temp[i] = colors[i];
-            colors = temp;
+        if(check_if_digit(colors[entries]) == true){
+            break;
+        }
+        entries++;
+
+        if(entries == space){
+            space += 1;
+            unique_ptr<string[]> temp = make_unique<string[]>(space);
+            copy(colors.get(), colors.get() + entries, temp.get());
+            colors = move(temp);
         }
     }
-    
 
-    for(int i = 0; i < entries; i++){
-        cout << sanity[i] << endl;// Delete after done using or you get memory leak
-         
+    for (int i = 0; i < entries; i++){
+        cout << colors[i] << endl;
     }
-    delete[] sanity;
-    
-    
+    cout << endl;
 
+}
+
+
+int main(){
+    int choice;
+    cout << "Choices" << endl << "1. Start Program" << endl << "2. Exit" << endl << "Choose a Number: ";
+    cin  >> choice;
+    cout << endl;
+
+    if(choice == 1){
+        cout << "List as many colors as you can. Type a number when you are done" << endl;
+        program();
+        main();
+    }
+    else if(choice == 2){
+        cout << "Program End";
+    }
+    else{
+        cout << "Not an Option" << endl;
+        main();
+    }
+    
     return 0;
-
 }
