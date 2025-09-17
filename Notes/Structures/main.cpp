87348d7b749602a74a87e2117b143935fc89075f
UCAS-Asher <asher.wangia@ucas-edu.net>
2025-09-17 11:47:41 -0600

    Working on It + Notes Done

--- Notes/Structures/main.cpp
+++ Notes/Structures/main.cpp
@@ -2,15 +2,19 @@
 
 
 //What are structures?
+    //a user-defined data type that groups together variables of different data types under a single name
 
 //How do you build a structure?
+    //use the struct keyword provide a name for your structure declare its variables of different types
 
 //What can be held in a structure?
     //Properties
 
 //How do you access the information in a structure?
+    //a dot between the structure  name and the variable you want to access
 
-//How do you overload an operator
+//How do you overload an operator?
+    //you define an operator function using the operator keyword followed by an operator symbol
 
 
 
@@ -19,6 +23,8 @@
 #include <vector>
 
 using namespace std;
+
+
 
 struct Date{
     short year = 1900;
@@ -31,14 +37,6 @@ struct Movie{
     Date releaseDate;
     bool isPopular = true;
 
-    bool ==(const Movie& movie){
-        if(title == movie.title && releaseDate.year == movie.releaseDate.year && releaseDate.month == movie.releaseDate.month && releaseDate.day == movie.releaseDate.day && isPopular == movie.isPopular){
-            return true;
-        }
-        else{
-            return false;
-        }
-    }
 };
 
 struct Customer{
@@ -46,6 +44,31 @@ struct Customer{
     int id;
     string email;
 };
+
+
+bool operator== (const Movie& first, const Movie& second){
+    return(first.title == second.title && 
+        first.releaseDate.year == second.releaseDate.year && 
+        first.releaseDate.month == second.releaseDate.month && 
+        first.releaseDate.day == second.releaseDate.day && 
+        first.isPopular == second.isPopular);
+}
+    
+
+ostream& operator<<(ostream& stream, Movie& movie){
+    stream << movie.title;
+    return stream;
+}
+
+ostream& operator<<(ostream& stream, Customer& customer){
+    stream << customer.name;
+    return stream;
+}
+
+
+void showMovie(Movie* movie){
+    cout << movie ->title << endl;
+}
 
 
 int main(){
@@ -58,6 +81,8 @@ int main(){
 
     Movie movie1 = {"Terminator", {1984, 6 ,1}};
     Movie movie2 = {"Terminator 2", 1987};
+    showMovie(&movie1);
+
 
     //auto[title, releaseYear, isPopular]{movie};
 
@@ -67,13 +92,18 @@ int main(){
         cout << "Movie Year: " << releaseDate.year << endl;
         cout << "Movie Popular: " << isPopular << endl;
     }
-    cout << movie1 == movie2 << endl;
+    if(movie1 == movie2){
+        cout << movie1 << "Equal" << endl;
+    }
+    else{
+        cout << movie1 << " is not " << movie2 << endl;
+    }
    
 
-    Customer cust;
-    cust.name = "Greg";
-    cust.id = 676767;
-    cust.email = "greg@gmail.com";
+    Customer customer;
+    customer.name = "Greg";
+    customer.id = 676767;
+    customer.email = "greg@gmail.com";
 
 
     return 0;
--- User_Structure/main.cpp
+++ User_Structure/main.cpp
@@ -4,12 +4,57 @@
 #include <string>
 using namespace std;
 
+struct User{
+    string name;
+    string password;
+    bool admin;
+};
+
+User user1 = {"Max", "I Love Mangos", false};
+User user2 = {"Mathew", "I Love Mustard", false};
+User user3 = {"Mark", "I Love 67", true};
+User user4 = {"Bob", "I Love Steak", false};
+User user5 = {"Silas", "I Love Chicken", true};
+User user6 = {"Alex", "I Love Cereal", false};
+User user7 = {"Carter", "I Love Fries", true};
+User user8 = {"", "I Love Mangos", false};
+
+
+void login(){
+
+}
+
+void sign_up(){
+
+}
+
+
 int main(){
+    int choice;
+    cout << "Choices" << endl << "1. Start Program" << endl << "2. Exit" << endl << "Choose a Number: ";
+    cin  >> choice;
+    cout << endl;
 
-
-
-
-
+    if(choice == 1){
+        int choice2;
+        cout << "Choices" << endl << "1. Login" << endl << "2. Sign Up" << endl << "Choose a Number: ";
+        cin >> choice2;
+        cout << endl;
+        if(choice2 == 1)
+            login();
+        else if(choice2 == 2)
+            sign_up();
+        else
+            cout << "Not an Option" << endl;
+        main();
+    }
+    else if(choice == 2){
+        cout << "Program End";
+    }
+    else{
+        cout << "Not an Option" << endl;
+        main();
+    }
 
 
     return 0;
