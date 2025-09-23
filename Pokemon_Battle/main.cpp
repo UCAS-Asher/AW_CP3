931fe04ccfa6941b05f44f07c243dd5457907a76
UCAS-Asher <asher.wangia@ucas-edu.net>
2025-09-23 11:49:04 -0600

    Working On It

--- Pokemon_Battle/main.cpp
+++ Pokemon_Battle/main.cpp
@@ -1,13 +1,15 @@
 //AW, Pokemon Battle
 
 #include <iostream>
+#include <thread>
+#include <chrono>
 
 using namespace std;
 
 enum Main{
     Explore = 1,
     Battle,
-    Items ,
+    Inventory,
     Exit
 };
 
@@ -19,8 +21,81 @@ enum Battle{
 
 };
 
+void print_one(string message){
+    for(int i = 0; i < size(message); i++){
+        cout << message[i];
+        this_thread::sleep_for(std::chrono::milliseconds(50));
+    }
+    cout << endl;
+}
+
+void explore(){
+
+}
+
+
+void battle(){
+
+}
+
+void items(){
+
+}
+
+
+
+
+
+
 int main(){
+    this_thread::sleep_for(std::chrono::milliseconds(1000));
+    print_one("Welcome to the world of pokemon! This world is inhabited by creatures called pokemon! For some people, they are pets. Others use them for fights. What will you use them for as you explore this world?");
+    print_one("Do you want to continue and explore this world(Y/N): ");
+    string opening;
 
+    if(!(cin >> opening)){
+        cout << "" << endl << endl;
+        cin.clear();
+        cin.ignore();
+        cout << "I will asume this is a Yes :)" << endl;
+    }
 
-    return 0;
+    if(opening == "Y"){
+        cout << "Nice Choice" << endl;
+    }
+    else if(opening == "N"){
+        cout << "Bye Bye :(" << endl;
+    }
+    else{
+        cout << "I will asume this is a Yes :)" << endl;
+    }
+   
+    while(true){
+        int choice;
+        cout << "Main Menu" << endl << "1. Explore" << endl << "2. Battle" << endl << "3. Items" << endl << "4. Exit" << endl << "Choose a Number: ";
+
+        while(!(cin >> choice)){
+            cout << "Put a Number!" << endl << endl;
+            cin.clear();
+            cin.ignore();
+            cout << "Main Menu" << endl << "1. Explore" << endl << "2. Battle" << endl << "3. Items" << endl << "4. Exit" << endl << "Choose a Number: ";
+        }
+
+        if(choice == Main::Explore){
+            explore();
+        }
+        else if(choice == Main::Battle){
+            battle();
+        }
+        else if(choice == Main::Inventory){
+            items();
+        }
+        else if(choice == Main::Exit){
+            cout << "Program End";
+            break;
+        }
+        else{
+            cout << "Not an Option!";
+        }
+   }
 }
