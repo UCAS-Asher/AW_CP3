f6ab261123b0a011c2890d862cf2b149248c7594
UCAS-Asher <asher.wangia@ucas-edu.net>
2025-09-23 17:07:57 +0000

    Working on It

---
+++ Pokemon_Battle/main.cpp
@@ -0,0 +1,26 @@
+//AW, Pokemon Battle
+
+#include <iostream>
+
+using namespace std;
+
+enum Main{
+    Explore = 1,
+    Battle,
+    Items ,
+    Exit
+};
+
+enum Battle{
+    Attack = 1,
+    Items,
+    Pokemon,
+    Run
+
+};
+
+int main(){
+
+
+    return 0;
+}
