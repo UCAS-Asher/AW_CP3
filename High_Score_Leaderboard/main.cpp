755d5644df4a0a94c226b814e06a245619a0c529
UCAS-Asher <asher.wangia@ucas-edu.net>
2025-10-02 21:35:18 -0600

    Finished

--- High_Score_Leaderboard/main.cpp
+++ High_Score_Leaderboard/main.cpp
@@ -68,26 +68,65 @@ vector <Score> update_scores(){
 }
 
 
-void add_score(){
+void add_score(string name, int score, string date, const vector <Score>& scores){
+    ofstream file("highscores.csv");
+    bool in_file = false;
 
+    if (file.is_open()) {
+        file << "Name,Score,Date\n";
+
+        for (const Score& comp_score : scores) {
+            if (!in_file && score > comp_score.score) {
+                file << name << "," << score << "," << date << "\n";
+                in_file = true;
+            }
+            file << comp_score.name << "," << comp_score.score << "," << comp_score.date << "\n";
+        }
+
+        
+        if (!in_file) {
+            file << name << "," << score << "," << date << "\n";
+        }
+        
+        cout << "Added Score!\n";
+        file.close();
+    }
+    else{
+        cout << "File does not exist!\n";
+    }
 }
 
 string get_date(){
     int month;
-    cout << "Month: ";
-    month = int_input_val(month, "Month: ");
-
+    while(month > 12 || month < 1){
+        cout << "Month: ";
+        month = int_input_val(month, "Month: ");
+        if(month > 12 || month < 1){
+            cout << "Put an actual Month!\n";
+        }
+    }
+    
+    
     int day;
-    cout << "Day: ";
-    day = int_input_val(day, "Day: ");
+    while(day > 31 || day < 1){
+        cout << "Day: ";
+        day = int_input_val(day, "Day: ");
+        if(day > 31 || day < 1){
+            cout << "Put an actual day!\n";
+        }
+    }
 
     int year;
     cout << "Year: ";
     year = int_input_val(year, "Year: ");
 
+
+    string date = to_string(month) + "/" + to_string(day) + "/" + to_string(year);
+
+    return date;
 }
 
-void get_score(){
+void get_score(const vector <Score>& scores){
     string name;
     cout << "Name: ";
     name = string_input_val(name, "Name: ");
@@ -96,9 +135,9 @@ void get_score(){
     cout << "Score: ";
     score = int_input_val(score, "Score: ");
 
-    string date;
-    cout << "Month: ";
-    date = string_input_val(date, "Date: ");
+    string date = get_date();
+    add_score(name, score, date, scores);
+    
 }
 
 void see_score(const vector <Score>& scores){
@@ -119,17 +158,17 @@ int main(){
         choice = int_input_val(choice, "High Score Choices\n 1. Add a Highscore\n 2. See High Scores\n 3. Exit\n Choose a Number: ");
 
         if(choice == 1){
-            get_score();
+            get_score(scores);
         }
         else if(choice == 2){
             see_score(scores);
         }
         else if(choice == 3){
             cout << "Bye Bye" << endl;
+            break;
         }
         else{
             cout << "Not an Option" << endl;
-            main();
         }
     }
     return 0;
