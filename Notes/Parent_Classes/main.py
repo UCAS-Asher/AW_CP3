8ee67e884e2eff6c9b963eef03d7d209caa42bfe
UCAS-Asher <asher.wangia@ucas-edu.net>
2025-10-13 11:48:33 -0600

    Working On It

---
+++ Notes/Parent_Classes/main.py
@@ -0,0 +1,20 @@
+#AW Parent Classes
+
+
+#What is a parent/abstract class?
+
+#How do you create a child class?
+
+#How does a child class inherit from the parent class?
+
+#What are class diagrams?
+
+#How are class diagrams used to show a parent/child relationship?
+
+#How do you overload operators in a class?
+
+#What are test cases?
+
+#Why do we use test cases?
+
+#How do we create test cases?
--- Student_Class/main.py
+++ Student_Class/main.py
@@ -1,32 +1,64 @@
 #AW Student  Class
 
+def is_numeric_string(variable):#Checks if the variable is a number
+    try:
+        int(variable)
+        return True
+    except ValueError:
+        print("Not a Number!")
+        return False
 
 class Student:
     student_id: int
     student_name: str
-    student_grade: float
+    student_grade: int
+    students = []
 
     def __init__(self, student_id=000, student_name="John Doe", student_grade=100):
         self.student_id = student_id
         self.student_name = student_name
         self.student_grade = student_grade
+        Student.students.append(self)#Adds the student to a list
 
-    def get_students():
-        pass
+    def get_students(self):
+        print(self)
 
-    def __str__(self):
+    def set_grade(self):
+        new_grade = input(f"{self.student_name}'s New Grade: ")
+        if is_numeric_string(new_grade) == True and new_grade <= 100 and new_grade >= 0:#Checks for valid grade change
+            self.student_grade = new_grade
+        else:
+            print("Not an Option!")
+            Student.change_grade(self)
+
+
+
+
+    def __str__(self,):
         return f"Student ID: {self.student_id}, Name: {self.student_name}, Grade: {self.student_grade}"
-    
+
+
+max = Student(0o1,"Max Well")
+bart = Student(0o2, "Bart Bort", 67)
+bob = Student(0o3, "Bob Spore", 76)
+mark = Student(0o4, "Mark Park", 87)
+john = Student()
 
 
 def main():
-    print("Choices\n1.Display Student Grades\n2.Modify Student Grades\n3.Exit")
+    print("Choices\n1.Display Students\n2.Modify Student Grades\n3.Exit")
     choice = input("Choose a Number: ")
 
     if choice == "1":
-        pass
+        for student in Student.students:
+            Student.get_students(student)
+            print("\n")
+        main()
     elif choice == "2":
-        pass
+        for student in Student.students:
+            Student.set_grade(student)
+        print("\n")
+        main()
     elif choice == "3":
         print("Program End!")
     else:
